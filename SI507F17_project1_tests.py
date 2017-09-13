4e5be7d779ff9892cb4e290fe03d463bad392677
Mengying Zhang <zhangmen@umich.edu>
2017-09-13 10:55:28 -0400

    Detected first bug in sort_cards()

--- SI507F17_project1_tests.py
+++ SI507F17_project1_tests.py
@@ -9,3 +9,111 @@ from SI507F17_project1_cards import *
 ## You should invoke the tests with verbosity=2 (make sure you invoke them!)
 
 ###########
+
+class TestCard(unittest.TestCase):
+	def setUp(self):
+		self.test_card = Card()
+		self.test_card1 = Card(0,1)
+
+	def test_constructor(self):
+    	# test for default constructor
+		self.assertEqual(self.test_card.suit, "Diamonds")
+		self.assertEqual(self.test_card.rank_num, 2)
+		self.assertEqual(self.test_card.rank, 2)
+        
+    	# test for non-default constructor
+		self.assertEqual(self.test_card1.suit, "Diamonds")
+		self.assertEqual(self.test_card1.rank_num, 1)
+		self.assertEqual(self.test_card1.rank, "Ace")
+
+	def tearDown(self):
+		pass
+
+
+class TestDeck(unittest.TestCase):
+	def setUp(self):
+		self.test_Deck = Deck()
+		self.test_Deck1 = Deck()
+
+		# Deck without the "Queen of Clubs" 
+		self.test_Deck2 = Deck()
+		self.test_Deck2.pop_card(24)
+
+		self.test_Deck3 = Deck()
+
+	def test_pop_card(self):
+    	# test pop_card with default input on full deck with 52 cards
+		self.test_Deck.pop_card()
+    	# pop one last card
+		self.assertEqual(len(self.test_Deck.cards),51)
+		self.assertEqual(self.test_Deck.cards[-1].suit,"Spades")
+    	# pop all cards
+		for i in range(0,50):
+			self.test_Deck.pop_card()
+		self.assertEqual(self.test_Deck.cards[0].rank_num,1)
+		self.assertEqual(self.test_Deck.cards[0].suit,"Diamonds")
+		self.test_Deck.pop_card()
+		self.assertEqual(len(self.test_Deck.cards),0)
+
+        # test pop_card with non-default input
+		self.test_Deck1.pop_card(5)
+		self.assertEqual(self.test_Deck1.cards[5].rank_num,7)
+		self.assertEqual(self.test_Deck1.cards[5].suit,"Diamonds")
+		self.test_Deck1.pop_card(0)
+		self.assertEqual(self.test_Deck1.cards[0].rank_num,2)
+		self.assertEqual(self.test_Deck1.cards[0].suit,"Diamonds")
+
+	def test_replace_card(self):
+    	# if the input Card is not in the Deck --> should be put back into the Deck
+		self.assertEqual(len(self.test_Deck2.cards),51)	
+		self.test_Deck2.replace_card(Card(1,12))
+		self.assertEqual(len(self.test_Deck2.cards),52)
+
+		# if the input Card is in the Deck --> no changes
+		self.test_Deck2.replace_card(Card(1,12))
+		self.test_Deck2.replace_card(Card(3,10))
+		self.assertEqual(len(self.test_Deck2.cards),52)
+
+	def test_sort_cards(self):
+		# sort with full Deck of cards
+		self.test_Deck3.shuffle()
+		self.test_Deck3.sort_cards()
+		self.assertEqual(self.test_Deck3.cards[0].suit,"Diamonds")
+		self.assertEqual(self.test_Deck3.cards[0].rank_num,1)
+		self.assertEqual(self.test_Deck3.cards[13].suit,"Clubs")
+		self.assertEqual(self.test_Deck3.cards[13].rank_num,1)
+		self.assertEqual(self.test_Deck3.cards[-3].suit,"Spades")
+		self.assertEqual(self.test_Deck3.cards[-3].rank_num,11)
+
+		# sort with non-full Deck of cards
+		self.test_Deck3.pop_card()
+		self.test_Deck3.pop_card()
+		self.assertEqual(self.test_Deck3.cards[-2].rank_num,10)
+		self.assertEqual(self.test_Deck3.cards[-2].suit,"Spades")
+		self.test_Deck3.pop_card(0)
+		self.test_Deck3.pop_card(5)
+		self.test_Deck3.pop_card(20)
+		self.test_Deck3.shuffle()
+		self.test_Deck3.sort_cards()
+		self.assertEqual(self.test_Deck3.cards[-2].rank_num,10)
+		self.assertEqual(self.test_Deck3.cards[-2].suit,"Spades")
+
+	def test_deal_hand():
+
+	def tearDown(self):
+		pass
+
+
+
+
+
+
+
+
+
+
+
+
+unittest.main(verbosity=2)
+
+## Bugs Detected! 1. sort_cards()
